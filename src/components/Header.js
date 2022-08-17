ee1dc53d358d38800fb8e04388d09336c40b84e7
omaran bazna <omran4d@gmail.com>
2022-08-17 19:30:38 -0400

    add content to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import "./styles/Header.css";
 const Header = () => {
-  return <div></div>;
+  return (
+    <div className="header">
+      <h2>This is a header</h2>
+    </div>
+  );
 };
 
 export default Header;
