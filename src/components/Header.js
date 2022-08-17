7aa65e31cb0c5c486e25898fc61ade9a45a6e728
omaran bazna <omran4d@gmail.com>
2022-08-17 19:37:02 -0400

    divide the header to sections

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./styles/Header.css";
+
 const Header = () => {
   return (
     <div className="header">
       <h2>This is a header</h2>
+      <div className="header-left"></div>
+      <div className="header-middle"></div>
+      <div className="header-right"></div>
     </div>
   );
 };
