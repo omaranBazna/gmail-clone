895bc639d9da54af24d75475966e292d0c927d82
omaran bazna <omran4d@gmail.com>
2022-08-17 19:41:21 -0400

    add the header menu button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./styles/Header.css";
-
+import MenuIcon from "@mui/icons-material/Menu";
+import { IconButton } from "@mui/material";
 const Header = () => {
   return (
     <div className="header">
       <h2>This is a header</h2>
-      <div className="header-left"></div>
+
+      <div className="header-left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png"
+          alt=""
+        />
+      </div>
       <div className="header-middle"></div>
       <div className="header-right"></div>
     </div>
