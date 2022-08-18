1e54ab473713c6eb71e49826f8af56505093559e
omaran bazna <omran4d@gmail.com>
2022-08-17 20:02:03 -0400

    add sidebar content

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,12 @@
 import "./styles/Sidebar.css";
 import React from "react";
-
+import { Button } from "@mui/material";
 const Sidebar = () => {
-  return <div></div>;
+  return (
+    <div className="sidebar">
+      <Button>Compose</Button>
+    </div>
+  );
 };
 
 export default Sidebar;
