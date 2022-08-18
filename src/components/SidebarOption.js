e2cdce968ef94c2ea4a4dd195dc5b74b3a77f230
omaran bazna <omran4d@gmail.com>
2022-08-17 20:13:23 -0400

    create sidebar option component

---
+++ src/components/SidebarOption.js
@@ -0,0 +1,8 @@
+import "./styles/SidebarOption.css";
+import React from "react";
+
+const SidebarOption = () => {
+  return <div className="sidebar-option"></div>;
+};
+
+export default SidebarOption;
