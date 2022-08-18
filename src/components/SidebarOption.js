1380186d5417ebcb7fdf84029883d8e5e942f365
omaran bazna <omran4d@gmail.com>
2022-08-17 20:17:06 -0400

    add content to the sidebar option

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -2,7 +2,13 @@ import "./styles/SidebarOption.css";
 import React from "react";
 
 const SidebarOption = ({ Icon, title, number }) => {
-  return <div className="sidebar-option"></div>;
+  return (
+    <div className="sidebar-option">
+      <Icon />
+      <p>{title}</p>
+      <p>{number}</p>
+    </div>
+  );
 };
 
 export default SidebarOption;
