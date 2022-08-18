5fef68bef69320ae97410c935b1cce2496d0dfd9
omaran bazna <omran4d@gmail.com>
2022-08-17 20:29:45 -0400

    add stared sidebar option

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Button } from "@mui/material";
 import { Add } from "@mui/icons-material";
 import InboxIcon from "@mui/icons-material/Inbox";
+import StarIcon from "@mui/icons-material/Inbox";
 import SidebarOption from "./SidebarOption";
 const Sidebar = () => {
   return (
@@ -12,6 +13,7 @@ const Sidebar = () => {
       </Button>
 
       <SidebarOption Icon={InboxIcon} title="Inbox" number={53} />
+      <SidebarOption Icon={StarIcon} title="Stared" number={23} />
     </div>
   );
 };
