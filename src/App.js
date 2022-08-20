0e4546bf5891c8c2777709d2d94696a4eaa89cae
omaran bazna <omran4d@gmail.com>
2022-08-20 01:15:19 -0400

    add the react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
-
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
