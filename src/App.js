526fc2617971e902b199322857e6e9bfaa08b8ee
omaran bazna <omran4d@gmail.com>
2022-08-20 01:19:07 -0400

    create email list component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import { Routes, Route } from "react-router-dom";
 import { Mail } from "@mui/icons-material";
+import EmailList from "./components/EmailList";
 function App() {
   return (
     <div className="App">
       <Header />
       <Sidebar />
       <Routes>
-        <Route path="/" element={} />
-         <Route path="/mail" element={<Mail />} />
+        <Route path="/" element={<EmailList />} />
+        <Route path="/mail" element={<Mail />} />
       </Routes>
     </div>
   );
---
+++ src/components/EmailList.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./style/email-list.css";
+const EmailList = () => {
+  return <div></div>;
+};
+
+export default EmailList;
