c19375648a4aa98676ae3a8544ca7ad38d93c234
omaran bazna <omran4d@gmail.com>
2022-08-20 01:22:16 -0400

    add checkbox

--- src/components/EmailList.js
+++ src/components/EmailList.js
@@ -1,10 +1,14 @@
 import React from "react";
+
+import { IconButton, Checkbox } from "@mui/material";
 import "./styles/emailList.css";
 const EmailList = () => {
   return (
     <div className="emailList">
       <div className="emailList-settings">
-        <div className="emailList-setting-left"></div>
+        <div className="emailList-setting-left">
+          <Checkbox />
+        </div>
       </div>
     </div>
   );
