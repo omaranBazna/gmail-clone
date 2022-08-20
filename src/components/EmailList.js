259b183b00b7aa2ab29085d4de9cfafedd8a808d
omaran bazna <omran4d@gmail.com>
2022-08-20 01:28:29 -0400

    import more icons to the mail list

--- src/components/EmailList.js
+++ src/components/EmailList.js
@@ -4,6 +4,9 @@ import { IconButton, Checkbox } from "@mui/material";
 import RedoIcon from "@mui/icons-material/Redo";
 import UndoIcon from "@mui/icons-material/Undo";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import SettingsIcon from "@mui/icons-material/Settings";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import "./styles/emailList.css";
 const EmailList = () => {
   return (
@@ -21,6 +24,7 @@ const EmailList = () => {
             <MoreVertIcon />
           </IconButton>
         </div>
+        <div className="emailList-setting-right"></div>
       </div>
     </div>
   );
