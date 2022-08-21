39aee22fb913ce2208490837de4886877a878f65
omaran bazna <omran4d@gmail.com>
2022-08-21 18:49:44 -0400

    import navigate

--- src/components/EmailRow.js
+++ src/components/EmailRow.js
@@ -3,8 +3,9 @@ import { IconButton, Checkbox } from "@mui/material";
 import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
 import LabelImportantOutlinedIcon from "@mui/icons-material/LabelImportantOutlined";
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 const EmailRow = ({ id, title, subject, description, time }) => {
+  const navigate = useNavigate();
   return (
     <div className="email-row">
       <div className="email-row-options">
