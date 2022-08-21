f368867986b4e1a325f111a587e5aaba6dfc5309
omaran bazna <omran4d@gmail.com>
2022-08-21 18:56:10 -0400

    add content to the mail

--- src/components/Mail.js
+++ src/components/Mail.js
@@ -1,7 +1,14 @@
 import React from "react";
 import "./styles/mail.css";
 const Mail = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="mail-tools">
+        <div className="mail-tools-left"></div>
+        <div className="mail-tools-right"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Mail;
