e8beea733b377bd427010966ce7a3637f7618782
omaran bazna <omran4d@gmail.com>
2022-08-21 18:32:20 -0400

    add Icons to email row options

--- src/components/EmailRow.js
+++ src/components/EmailRow.js
@@ -1,10 +1,22 @@
 import "./styles/email-row.css";
+import { IconButton } from "@mui/material";
+import Checkbox from "@mui/material";
+import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
+import LabelImportantOutlinedIcon from "@mui/icons-material/LabelImportantOutlined";
 import React from "react";
 
 const EmailRow = ({ id, title, subject, description, time }) => {
   return (
     <div className="email-row">
-      <div className="email-row-options"></div>
+      <div className="email-row-options">
+        <Checkbox />
+        <IconButton>
+          <StarBorderOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <LabelImportantOutlinedIcon />
+        </IconButton>
+      </div>
       <div className="email-row-title"></div>
       <div className="email-row-message"></div>
       <div className="email-row-description"></div>
