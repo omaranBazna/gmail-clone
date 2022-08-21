bde174e9ea4ddb7a0c5054cd1845d950db9dd368
omaran bazna <omran4d@gmail.com>
2022-08-21 19:58:22 -0400

    add Icons to the email options

--- src/components/Mail.js
+++ src/components/Mail.js
@@ -1,10 +1,38 @@
 import React from "react";
 import "./styles/mail.css";
+
+import { IconButton } from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import MoveToInboxIcon from "@mui/icons-material/MoveToInbox";
+import ErrorIcon from "@mui/icons-material/Error";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EmailIcon from "@mui/icons-material/Email";
+import WatchLaterIcon from "@mui/icons-material/WatchLater";
 const Mail = () => {
   return (
     <div>
       <div className="mail-tools">
-        <div className="mail-tools-left"></div>
+        <div className="mail-tools-left">
+          <IconButton>
+            <ArrowBackIcon />
+          </IconButton>
+          <IconButton>
+            <MoveToInboxIcon />
+          </IconButton>
+          <IconButton>
+            <ErrorIcon />
+          </IconButton>
+
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+          <IconButton>
+            <EmailIcon />
+          </IconButton>
+          <IconButton>
+            <WatchLaterIcon />
+          </IconButton>
+        </div>
         <div className="mail-tools-right"></div>
       </div>
     </div>
