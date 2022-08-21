6368a75d59e8afd1a58b62bd258ca0f579cacac5
omaran bazna <omran4d@gmail.com>
2022-08-21 18:29:34 -0400

    add email row parts

--- src/components/EmailRow.js
+++ src/components/EmailRow.js
@@ -2,7 +2,14 @@ import "./styles/email-row.css";
 import React from "react";
 
 const EmailRow = ({ id, title, subject, description, time }) => {
-  return <div className="email-row"></div>;
+  return (
+    <div className="email-row">
+      <div className="email-row-options"></div>
+      <div className="email-row-title"></div>
+      <div className="email-row-message"></div>
+      <div className="email-row-description"></div>
+    </div>
+  );
 };
 
 export default EmailRow;
