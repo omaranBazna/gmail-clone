8c5f3e1ab0236cdd179f7b83d5be495c2473a5b8
omaran bazna <omran4d@gmail.com>
2022-08-21 20:02:24 -0400

    add more icons to the mail component

--- src/components/Mail.js
+++ src/components/Mail.js
@@ -8,6 +8,8 @@ import ErrorIcon from "@mui/icons-material/Error";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EmailIcon from "@mui/icons-material/Email";
 import WatchLaterIcon from "@mui/icons-material/WatchLater";
+import LabelImportantIcon from "@mui/icons-material/LabelImportant";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 const Mail = () => {
   return (
     <div>
@@ -31,6 +33,12 @@ const Mail = () => {
           </IconButton>
           <IconButton>
             <WatchLaterIcon />
+          </IconButton>
+          <IconButton>
+            <LabelImportantIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
           </IconButton>
         </div>
         <div className="mail-tools-right"></div>
