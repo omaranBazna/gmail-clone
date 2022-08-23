f7012a9f52c5359129fe4d2f434c593198c6fa69
omaran bazna <omran4d@gmail.com>
2022-08-22 21:34:39 -0400

    add the user reducer to the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { mailReducer } from "./features/mailSlice";
+import { userReducer } from "./features/useSlice";
 export default configureStore({
   reducer: {
     mail: mailReducer,
+    user: userReducer,
   },
 });
