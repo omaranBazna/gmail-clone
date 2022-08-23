93d21c99bb19e1c17448a58c85b682c128d56029
omaran bazna <omran4d@gmail.com>
2022-08-22 21:37:49 -0400

    handle no login status

--- src/App.js
+++ src/App.js
@@ -6,20 +6,28 @@ import Mail from "./components/Mail";
 import EmailList from "./components/EmailList";
 import { useSelector } from "react-redux";
 import SendMail from "./components/SendMail";
+
 function App() {
   const send = useSelector((state) => state.mail.sendMessage_isOpen);
+
+  const userEmail = useSelector((state) => state.user.userEmail);
   return (
     <div className="App">
       <Header />
-      <div className="app-body">
-        <Sidebar />
-        <Routes>
-          <Route path="/" element={<EmailList />} />
-          <Route path="/mail" element={<Mail />} />
-        </Routes>
 
-        {send && <SendMail />}
-      </div>
+      {userEmail !== "" && (
+        <div className="app-body">
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<EmailList />} />
+            <Route path="/mail" element={<Mail />} />
+          </Routes>
+
+          {send && <SendMail />}
+        </div>
+      )}
+
+      {userEmail === "" && <button>Login</button>}
     </div>
   );
 }
